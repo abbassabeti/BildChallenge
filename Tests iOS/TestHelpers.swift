68eb6204f48a57e441ea7ef092aee48d17cd5d9b
Abbas SabetiNezhad <a.sabetinezhad@tricentis.com>
2022-01-02 15:05:57 +0100

    Fixed: working on tests

---
+++ Tests iOS/TestHelpers.swift
@@ -0,0 +1,24 @@
+//
+//  TestHelpers.swift
+//  Tests iOS
+//
+//  Created by Abbas Sabeti on 02/01/2022.
+//
+
+import XCTest
+import ViewInspector
+@testable import BildChallenge
+
+
+enum MockError: Swift.Error {
+    case valueNotSet
+    case codeDataModel
+}
+
+extension NSError {
+    static var test: NSError {
+        return NSError(domain: "test", code: 0, userInfo: [NSLocalizedDescriptionKey: "Test error"])
+    }
+}
+
+extension Inspection: InspectionEmissary where V: Inspectable { }
